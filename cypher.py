13166fe4f6b711407a1c5b5dc620f608e5f1dc1f
DunkMB <info@bdne.net.au>
2026-08-25 20:47:30 +1000

    Update cypher.py

--- cypher.py
+++ cypher.py
@@ -15,24 +15,25 @@ print(" " * 50)
 print("_" * 50)
 print("Question 1")
 print("_" * 50)
-def cypher(char, shift1, shift2):
-    if char.isupper():
-        return chr((ord(char) - ord('A') + shift1 + shift2) % 13 + ord('A'))
-    elif char.islower():
-        return chr((ord(char) - ord('a') + shift1 * shift2) % 26 + ord('a'))
-    else:
-        return char  # leave numbers, spaces, punctuation unchanged
 
-def encode_text(text):
-    result = ""
-    for char in text:
-        if char.isupper():
-            result += cypher(char, 2, 4)
-        elif char.islower():
-            result += cypher(char, 2, 4)
-        else:
-            result += char
-    return result
+
+def cypher(char, shift1, shift2):
+    if char.islower():
+        index = ord(char) - ord('a')  
+        if index < 13:                                               # a-m
+            return chr((index + (shift1 * shift2) % 26 + ord('a')))
+        else index >= 13:                                            # n-z
+            return chr((index - (shift1 + shift2) % 26 + ord('a')))
+    elif char.isupper():
+        index = ord(char) - ord('A')
+        if index < 13:                                               # A-M
+            return chr((index - shift1) % 26 + ord('A'))
+        else:                                                        # N-Z
+            return chr((index + (shift2 * shift2) % 26 + ord('A')))
+    elif char.isdigit():
+        return str((int(char) + shift1 - shift2) % 10)    
+    else:
+        return char
 
 def main():
     input_filename = input("Filename to read: ")
